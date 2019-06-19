45dd34737f395883404374d8149f747b566a7b5f
Nathan Fowler <luxurysandbox@gmail.com>
2019-06-18 18:52:14 -1000

    added post endpoint

--- blog_server/src/app.js
+++ blog_server/src/app.js
@@ -7,6 +7,7 @@ const { NODE_ENV } = require('./config')
 const ArticlesService = require('./articles-service')
 
 const app = express()
+const jsonParser = express.json()
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
@@ -35,6 +36,22 @@ app.get('/articles/:article_id', (req, res, next) => {
         })
       }
       res.json(article)
+    })
+    .catch(next)
+})
+
+app.post('/articles', jsonParser, (req, res, next) => {
+  const { title, content, style } = req.body
+  const newArticle = { title, content, style }
+  ArticlesService.insertArticle(
+    req.app.get('db'),
+    newArticle
+  )
+    .then(article => {
+      res
+        .status(201)
+        .location(`/articles/${article.id}`)
+        .json(article)
     })
     .catch(next)
 })
